d545e45edd7c9929a056dab6973e07b8ff1c2cd6
Jean Dessane <jean.dessane@gmail.com>
2018-01-25 11:06:07 +0000

    first commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
