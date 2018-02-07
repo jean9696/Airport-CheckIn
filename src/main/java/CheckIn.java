8298aa8ddc7acdded37bf9b6445dc7bd9a958437
Christoffer Carlé Christensen <gofling2@hotmail.com>
2018-02-07 20:38:52 +0000

    File Reading

--- src/main/java/CheckIn.java
+++ src/main/java/CheckIn.java
@@ -2,6 +2,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
+import java.io.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import static java.lang.Integer.parseInt;
+
 public class CheckIn {
     private HashMap<String, Flight> flights;
 
@@ -42,11 +50,75 @@ public class CheckIn {
 
 
     public static HashMap<String, Flight> readFlightsFromInputFiles() {
-        return new HashMap<String, Flight>();
+        JSONParser jsonParser = new JSONParser();
+        HashMap<String, Flight> flightHashMap = new HashMap<String, Flight>();
+        try  {
+            JSONArray flights = (JSONArray) jsonParser.parse(new FileReader("../../../flights.json"));
+
+            for (Object o : flights)
+            {
+                JSONObject flight = (JSONObject) o;
+
+                String flightCode = (String) flight.get("Flight Code");
+
+                String destinationAirport = (String) flight.get("Destination Airport");
+                System.out.println(destinationAirport);
+
+                String carrier = (String) flight.get("Carrier");
+                System.out.println(carrier);
+
+                int maximumPassenger = parseInt((String) flight.get("Maximum Passengers"));
+                int maximumBaggageWeight = parseInt((String) flight.get("Maximum Baggage Weight"));
+                int maximumBaggageVolume = parseInt((String) flight.get("Maximum Baggage Volume"));
+
+                BaggageSize baggage = new BaggageSize(maximumBaggageWeight, maximumBaggageVolume);
+                Flight flightObject = new Flight(flightCode, destinationAirport, carrier, maximumPassenger, baggage);
+
+                flightHashMap.put(flightObject.getFlightCode(), flightObject);
+        }
+        } catch (Exception e) {
+
+        }
+        return flightHashMap;
     }
 
     public static HashMap<String, Booking> readBookingsFromInputFiles() {
+
+        JSONParser jsonParser = new JSONParser();
+        HashMap<String, Booking> bookingHashMap = new HashMap<String, Booking>();
+
+        try  {
+            JSONArray bookings = (JSONArray) jsonParser.parse(new FileReader("../../../bookings.json"));
+
+            for (Object o : bookings)
+            {
+                JSONObject booking = (JSONObject) o;
+
+                String passengerName = (String) booking.get("Passenger Name");
+                System.out.println(passengerName);
+
+                String bookingReferenceCode = (String) booking.get("Booking Reference Code");
+                System.out.println(bookingReferenceCode);
+
+                int flightCode = parseInt((String) booking.get("Flight Code"));
+                Boolean checkedIn = Boolean.valueOf((String) booking.get("Checked In"));
+
+                int baggageWeight = parseInt((String) booking.get("Baggage Weight"));
+                int baggageVolume = parseInt((String) booking.get("Baggage Volume"));
+
+                Flight flight = flights.get();
+
+                Passenger passenger = new Passenger("Bla", "Bla", 13);
+
+                BaggageSize baggage = new BaggageSize(baggageWeight, baggageVolume);
+                Booking bookingObject = new Booking(1, checkedIn, flightCode, baggage, passenger);
+                bookingHashMap.put(bookingReferenceCode, );
+            }
+        } catch (Exception e) {
+
+        }
         return new HashMap<String, Booking>();
+
     }
 
     public static void main (String[] args) {
---
+++ src/test/java/CheckInTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckInTest {
+
+    @Test
+    public void readFlightsFromInputFiles() {
+    }
+
+    @Test
+    public void readBookingsFromInputFiles() {
+    }
+}
